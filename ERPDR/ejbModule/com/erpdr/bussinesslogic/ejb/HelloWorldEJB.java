63849d8efd8785d272db84ddcd4ca10ab8ff8ecb
rirahetaGH <rutilio.iraheta@gmail.com>
2015-08-20 19:46:07 -0600

    initial version

---
+++ ERPDR/ejbModule/com/erpdr/bussinesslogic/ejb/HelloWorldEJB.java
@@ -0,0 +1,18 @@
+package com.erpdr.bussinesslogic.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldEJB
+ */
+@Stateless
+public class HelloWorldEJB implements HelloWorldEJBRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ERPDRClient/ejbModule/com/erpdr/bussinesslogic/ejb/HelloWorldEJBRemote.java
@@ -0,0 +1,8 @@
+package com.erpdr.bussinesslogic.ejb;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface HelloWorldEJBRemote {
+
+}
