0c964b533a08323e12e0674e0611e99054fdeed8
rirahetaGH <rutilio.iraheta@gmail.com>
2015-08-20 19:23:19 -0600

    start

---
+++ ERPDR/ejbModule/com/erpdr/ejb/BussinessLogic.java
@@ -0,0 +1,18 @@
+package com.erpdr.ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class BussinessLogic
+ */
+@Stateless
+public class BussinessLogic implements BussinessLogicRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public BussinessLogic() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ ERPDRClient/ejbModule/com/erpdr/ejb/BussinessLogicRemote.java
@@ -0,0 +1,8 @@
+package com.erpdr.ejb;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface BussinessLogicRemote {
+
+}
